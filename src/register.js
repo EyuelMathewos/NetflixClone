e512408cc1c3e5e02126135a44a17040927217fb
EyuelMathewos <31162254+EyuelMathewos@users.noreply.github.com>
2021-03-30 15:36:12 +0300

    Update register.js

--- src/register.js
+++ src/register.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import  {  ATag, Nav, NavBrand, FormInput, FullImage }  from  "./components/Container";
 import {Button} from "./components/Button";
 import netflixlogo from './components/img/netflixlogonew.png'
-
+let values={};
 export class register extends React.Component {
     constructor(props) {
         super(props);
